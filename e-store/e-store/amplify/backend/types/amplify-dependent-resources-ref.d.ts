1af3a6b1dbd445950b173cd40d9fe631e1b75e29
Priya Sharma <ps.21041998@gmail.com>
2025-01-06 20:56:50 +0530

    Added apple e-store app to React-Library

---
+++ e-store/e-store/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "estorereactdd304e90": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
